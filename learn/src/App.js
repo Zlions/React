42904b8653f2a6bb67bb272c694427091c7e3d3a
Zlions <2589689990@qq.com>
2020-08-27 09:28:31 +0800

    4.2

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
+    const [n, setN] = useState(0)
     return (
         <div>
+            <button onClick={() => {
+                setN(n - 1)
+            }}>-</button>
+            <span>{n}</span>
+            <button onClick={() => {
+                setN(n + 1)
+            }}>+</button>
         </div>
     )
 }
