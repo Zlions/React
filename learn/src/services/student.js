2135d7638811ed040dfded4ae7dac3005b922dac
Zlions <2589689990@qq.com>
2020-08-31 09:01:25 +0800

    master

--- learn/src/services/student.js
+++ learn/src/services/student.js
@@ -4,12 +4,12 @@ const appkey = "demo13_1545210570249";
  * 获取所有学生
  */
 export async function getAllStudents() {
-    return await fetch("/api/student/findAll?appkey=" + appkey)
+    return await fetch("http://open.duyiedu.com/api/student/findAll?appkey=" + appkey)
         .then(resp => resp.json()).then(resp => resp.data);
 }
 
 export async function getStudents(page = 1, limit = 10) {
-    return await fetch(`/api/student/findByPage?appkey=${appkey}&page=${page}&size=${limit}`)
+    return await fetch(`http://open.duyiedu.com/api/student/findByPage?appkey=${appkey}&page=${page}&size=${limit}`)
         .then(resp => resp.json()).then(resp => resp.data);
 }
 
@@ -22,7 +22,7 @@ export async function searchStudents(
     { page = 1, limit = 10, key = "", sex = -1 } = {}) {
     if (key) {
         //搜索
-        const resp = await fetch(`/api/student/searchStudent?appkey=${appkey}&page=${page}&size=${limit}&search=${key}&sex=${sex}`)
+        const resp = await fetch(`http://open.duyiedu.com/api/student/searchStudent?appkey=${appkey}&page=${page}&size=${limit}&search=${key}&sex=${sex}`)
             .then(resp => resp.json()).then(resp => resp.data);
         resp.datas = resp.searchList;
         delete resp.searchList;
