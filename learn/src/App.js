69d5aea1021223c2f1c4c7686b03f0b329188ef3
Zlions <2589689990@qq.com>
2020-08-31 10:57:43 +0800

    4.12

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect, useRef, useLayoutEffect} from 'react'
 export default function App() {
+    const [n, setN] = useState(0)
+    // useEffect(() => {
+    //     h1Ref.current.innerText = '123'
+    // })
+
+
+    useLayoutEffect(() => {
+        h1Ref.current.innerText = '123'
+    })
+    const h1Ref = useRef();
+
     return (
-        <div></div>
+        <div>
+            <h1 ref={h1Ref}>{n}</h1>
+            <button onClick={() => {
+                setN(n + 1)
+            }}>click</button>
+        </div>
     )
 }
