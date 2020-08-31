22fde69b4ab4cca665f2861d6c1d95564a900b02
Zlions <2589689990@qq.com>
2020-08-31 11:04:54 +0800

    4.13

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect, useDebugValue} from 'react'
+
+
+function useTest () {
+    const [stu, ] = useState([])
+    useDebugValue(stu)
+    return stu;
+}
+
+
+
 export default function App() {
+    useState(0);
+    useEffect(() => {
+        console.log("effect");
+    }, [])
+    useEffect(() => {
+        console.log("abc");
+    }, [])
+
+    useTest();
+
+
     return (
         <div></div>
     )
