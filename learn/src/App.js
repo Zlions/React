26da63110fa68f2ca56ae34ec536c75588ed781b
Zlions <2589689990@qq.com>
2020-08-31 09:27:32 +0800

    4.7

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+const ctx = React.createContext();
+
+
+// function Test() {
+//     return <ctx.Consumer>
+//         {value => <h1>{value}</h1>}
+//     </ctx.Consumer>
+// }
+
+function Test() {
+
+    const value = useContext(ctx);
+    return <h1>
+        {value}
+    </h1>
+}
+
+
 export default function App() {
     return (
-        <div></div>
+        <div>
+            <ctx.Provider value="abc">
+                <Test />
+            </ctx.Provider>
+        </div>
     )
 }
