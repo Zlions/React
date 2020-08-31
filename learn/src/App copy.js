0d32ac66b5e798793d855e7114cddf2ed89e00aa
Zlions <2589689990@qq.com>
2020-08-31 09:50:20 +0800

    4.8_CallbackHook

--- learn/src/App copy.js
+++ learn/src/App copy.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
 
 class Test extends React.PureComponent {
     render() {
@@ -15,17 +15,13 @@ export default function App() {
     console.log("app render");
     const [txt, setTxt] = useState(123)
     const [n, setN] = useState(0)
-    const handleClick = useCallback(
-        () => {
-            setTxt(txt + 1)
-        },
-        [txt]
-    )
 
     return (
         <div>
             {/* 函数地址每次渲染都发生了变化，导致子组件跟着重渲染，若子组件是优化过的，则可能导致优化失效 */}
-            <Test text={txt} onClick={handleClick} />
+            <Test text={txt} onClick={() => {
+                setTxt(123)
+            }} />
 
             <input type="number" name="" id="" value={n} onChange={e => {
                 setN(e.target.value)
