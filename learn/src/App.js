c6d6c1c6283808493e974be5e6d8a76511990826
Zlions <2589689990@qq.com>
2020-09-01 07:44:14 +0800

    4.14

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,6 +1,53 @@
-import React from 'react'
-export default function App() {
-    return (
-        <div></div>
-    )
-}
+// Transition
+
+// import { Transition } from 'react-transition-group';
+// import React, { useState } from 'react'
+
+// const duration = 3000;
+
+// const defaultStyle = {
+//     transition: `opacity ${duration}ms ease-in-out`,
+//     opacity: 0,
+// }
+
+// const transitionStyles = {
+//     entering: { opacity: 1 },
+//     entered: { opacity: 1 },
+//     exiting: { opacity: 0 },
+//     exited: { opacity: 0 },
+// };
+
+// const Fade = ({ in: inProp }) => (
+//     <Transition in={inProp} timeout={duration}>
+//         {state => (
+//             <div style={{
+//                 ...defaultStyle,
+//                 ...transitionStyles[state]
+//             }}>
+//                 I'm a fade Transition!
+//             </div>
+//         )}
+//     </Transition>
+// );
+
+// export default function App() {
+//     const [inProp, setInProp] = useState(false);
+//     return (
+//         <div>
+//             <Transition mountOnEnter unmountOnExit in={inProp} timeout={duration}>
+//                 {state => {
+//                     return (
+//                     <div style={{
+//                         ...defaultStyle,
+//                         ...transitionStyles[state]
+//                     }}>
+//                         I'm a fade Transition!
+//                     </div>
+//                 )}}
+//             </Transition>
+//             <button onClick={() => setInProp(!inProp)}>
+//                 Click to Enter
+//             </button>
+//         </div>
+//     );
+// }
